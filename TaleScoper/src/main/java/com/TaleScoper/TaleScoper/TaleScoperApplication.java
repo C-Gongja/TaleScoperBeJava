3760627dbec35d69cf047c8f0cc3bec67793cd2b
Taeyoon <taeyoonkim1995@gmail.com>
2025-09-17 19:23:37 -0500

    spring boot mvn init

---
+++ TaleScoper/src/main/java/com/TaleScoper/TaleScoper/TaleScoperApplication.java
@@ -0,0 +1,13 @@
+package com.TaleScoper.TaleScoper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleScoperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaleScoperApplication.class, args);
+	}
+
+}
---
+++ TaleScoper/src/test/java/com/TaleScoper/TaleScoper/TaleScoperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaleScoper.TaleScoper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaleScoperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
